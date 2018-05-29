31fbc239b39f053a4bd73d260afbc0fc389f0777
Altrim <altr1m1@hotmail.com>
2018-05-29 23:36:01 +0200

    code changed

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.45.0/mapbox-gl.js'></script>
+    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.45.0/mapbox-gl.css' rel='stylesheet' />
     <title>Test</title>
 
  <?php require('db/readdb.php'); ?>
@@ -14,6 +17,7 @@
 <form name="searchbox" action="/" target="" method="post">
     <input type="text" autocomplete="off" placeholder="Search..." name="mapinput" id="pac-input" />
     <input type="submit" style="height: 0px; width: 0px; border: none; padding: 0px;" hidefocus="true" />
+    
 </form>
 
   <section class="inmap" id="inmap" >
@@ -30,7 +34,9 @@
 								<img src="images/<?php echo $dok->image; ?>"></img><br><br>
                 <span class="copy-text"><?php echo $dok->description; ?></span><br><br>
                 <span class="copy-text"><?php echo $dok->address; ?></span><br><br>
-                <span class="copy-text">Tel: <?php echo $dok->tel; ?></span>
+                <span class="copy-text">Tel: <?php echo $dok->tel; ?></span><br><br>
+                <a href="mailto:<?php echo $dok->email; ?>" style="color: #1a75ac;"><i class="fas fa-envelope-square" style="font-size: 25px;"></i></a>&nbsp;
+                <a href="mailto:<?php echo $dok->website; ?>" style="color: #1a75ac;"><i class="fas fa-globe" style="font-size: 25px;"></i></a>
 							</li>
               </div>
               <?php } ?>		   
@@ -53,7 +59,6 @@ for (var i = 0; i < conte.length; i++) {
 }
 }
 </script>
-
 <script>
 var coll = document.getElementsByClassName("collapsible");
 var i;
@@ -79,8 +84,30 @@ for (i = 0; i < coll.length; i++) {
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 12,
       center: new google.maps.LatLng(53.594780, 9.884969),
+      container: 'map',
+      style: 'mapbox://styles/mapbox/basic-v9',
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
+
+
+        /*var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+        var icons = {
+          parking: {
+            icon: iconBase + 'parking_lot_maps.png'
+          },
+          library: {
+            icon: iconBase + 'library_maps.png'
+          },
+          info: {
+            icon: iconBase + 'info-i_maps.png'
+          }
+        };     
+        var features = [
+          {
+            position: new google.maps.LatLng(53.594780, 9.884969),
+            type: 'parking'
+          }];*/
+
 
     var infowindow = new google.maps.InfoWindow();
     var marker, i;
@@ -88,6 +115,7 @@ for (i = 0; i < coll.length; i++) {
   for (var i = 0; i < markersData.length; i++){
     marker = new google.maps.Marker({
         position: new google.maps.LatLng(markersData[i].lat, markersData[i].lng),
+        icon: 'images/'+markersData[i].mapicon,
         map: map
       });
 
