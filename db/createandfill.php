cd55ba3b064807e2e94728be7eb7f43469bbec27
Altrim <altr1m1@hotmail.com>
2018-06-02 20:14:39 +0200

    code changed

--- db/createandfill.php
+++ db/createandfill.php
@@ -24,16 +24,18 @@ if ($conn->query($sql) === TRUE) {
 // Create table
 $sql = "CREATE TABLE IF NOT EXISTS $tblName(
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    `logo` VARCHAR( 300 ) NOT NULL ,
-    `name` VARCHAR( 80 ) NOT NULL ,
+    `logo` VARCHAR( 100 ) NOT NULL ,
+    `image` VARCHAR( 100 ) NOT NULL ,
+    `mapicon` VARCHAR( 100 ) NOT NULL ,
+    `name` VARCHAR( 50 ) NOT NULL ,
     `description` VARCHAR( 300 ) NOT NULL ,
-    `tel` VARCHAR( 80 ) NOT NULL ,
-    `email` VARCHAR( 80 ) NOT NULL ,
-    `website` VARCHAR( 80 ) NOT NULL ,
-    `address` VARCHAR( 80 ) NOT NULL ,
+    `tel` VARCHAR( 50 ) NOT NULL ,
+    `email` VARCHAR( 50 ) NOT NULL ,
+    `website` VARCHAR( 50 ) NOT NULL ,
+    `address` VARCHAR( 100 ) NOT NULL ,
     `lat` FLOAT( 10, 6 ) NOT NULL ,
     `lng` FLOAT( 10, 6 ) NOT NULL ,
-    `type` VARCHAR( 30 ) NOT NULL
+    `type` VARCHAR( 50 ) NOT NULL
 )";
 if ($conn->query($sql) === TRUE) {
     echo "Table created successfully. \r\n";
@@ -42,7 +44,18 @@ if ($conn->query($sql) === TRUE) {
     echo "Error creating table: " . $conn->error;
 }
 
-$sql = "INSERT INTO $tblName (name,address,lat,lng,type) VALUES('nam1','hamburg1','53.594731','9.885090','lvl1'),('nam2','hamburg2','53.584731','9.875090','lvl2'),('nam3','hamburg3','53.574731','9.865090','lvl3'),('nam4','hamburg4','53.564731','9.855090','lvl4')";
+$sql = "INSERT INTO $tblName (logo, image, mapicon, name, description, tel, email, website, address, lat, lng, type) 
+VALUES
+('siegel_gold@2x.png','image.jpg','pin_gold.png','Dr. Med. Praxis fur Lorem ipsum 1','Lorem Ipsum has been the industrys standard dummy text ever since the 1500s','+49 123 456789','email@email.com','www.test.com','Hamburg, Germany','53.594730','9.885090','lvl1'),
+('siegel_bronze@2x.png','image.jpg','pin_bronze.png','Dr. Med. Praxis fur Lorem ipsum 2','Lorem Ipsum has been the industrys standard dummy text ever since the 1500s','+49 123 456789','email@email.com','www.test.com','Hamburg, Germany','53.554730','9.855090','lvl2'),
+('siegel_silber@2x.png','image.jpg','pin_silber.png','Dr. Med. Praxis fur Lorem ipsum 3','Lorem Ipsum has been the industrys standard dummy text ever since the 1500s','+49 123 456789','email@email.com','www.test.com','Hamburg, Germany','53.514730','9.815090','lvl3'),
+('siegel_gold@2x.png','image.jpg','pin_gold.png','Dr. Med. Praxis fur Lorem ipsum 4','Lorem Ipsum has been the industrys standard dummy text ever since the 1500s','+49 123 456789','email@email.com','www.test.com','Hamburg, Germany','53.414730','9.415090','lvl4'),
+('siegel_gold@2x.png','image.jpg','pin_gold.png','Dr. Med. Praxis fur Lorem ipsum 5','Lorem Ipsum has been the industrys standard dummy text ever since the 1500s','+49 123 456789','email@email.com','www.test.com','Hamburg, Germany','53.541196','9.984025','lvl5'),
+('siegel_gold@2x.png','image.jpg','pin_gold.png','Dr. Med. Praxis fur Lorem ipsum 6','Lorem Ipsum has been the industrys standard dummy text ever since the 1500s','+49 123 456789','email@email.com','www.test.com','Berlin, Germany','52.541521','13.279337','lvl6'),
+('siegel_gold@2x.png','image.jpg','pin_gold.png','Dr. Med. Praxis fur Lorem ipsum 7','Lorem Ipsum has been the industrys standard dummy text ever since the 1500s','+49 123 456789','email@email.com','www.test.com','Berlin, Germany','52.514476','13.242741','lvl7'),
+('siegel_bronze@2x.png','image.jpg','pin_bronze.png','Dr. Med. Praxis fur Lorem ipsum 8','Lorem Ipsum has been the industrys standard dummy text ever since the 1500s','+49 123 456789','email@email.com','www.test.com','Berlin, Germany','52.507924','13.335856','lvl8'),
+('siegel_silber@2x.png','image.jpg','pin_silber.png','Dr. Med. Praxis fur Lorem ipsum 9','Lorem Ipsum has been the industrys standard dummy text ever since the 1500s','+49 123 456789','email@email.com','www.test.com','Berlin, Germany','52.507924','13.335856','lvl9'),
+('siegel_bronze@2x.png','image.jpg','pin_bronze.png','Dr. Med. Praxis fur Lorem ipsum 10','Lorem Ipsum has been the industrys standard dummy text ever since the 1500s','+49 123 456789','email@email.com','www.test.com','Berlin, Germany','52.477252','13.392149','lvl10')";
 if ($conn->query($sql) === TRUE) {
     echo "Table filled successfully.";
 } else {
--- index.php
+++ index.php
@@ -4,14 +4,12 @@
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="js/fillmap.js"></script>
     <title>Search Map</title>
-
- <?php require('db/readdb.php'); ?>
-
-  </head>
-  <body style="margin:0px; padding:0px;">
-
+    <?php require('db/readdb.php'); ?> <!-- FILLS LEFT LIST BY DEFAULT -->
+</head>
+<body style="margin:0px; padding:0px;">
 
 <form class="searchform" name="searchbox" action="" target="" method="post">
     <img src="images/searchicon.png"/><br>
@@ -56,175 +54,6 @@
           </div>
           <div class="rightmapinfo" id="map"></div>
    </section>
-<script>
-var btnact = "";
-function myFunction() {
-/* DEACTIVATE ACTVIE CLASS */
-var conte = document.getElementsByClassName("content");
-btnact = document.getElementsByClassName("collapsible");
-for (var i = 0; i < conte.length; i++) {
-  conte[i].style.display = 'none';
-  btnact[i].className = btnact[i].className.replace(" active", "");
-}
-activateTab();
-/* END DEACTIVATE ACTVIE CLASS */
-}
-
-function activateTab() {
-/* ACTIVATE CURRENT CLASS */
-for (var i = 0; i < btnact.length; i++) {
-  btnact[i].addEventListener("click", function() { 
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
-}
-/* END ACTIVATE CURRENT CLASS */
-}
-</script> 
-<!-- CATEGORY MAP MARKERS -->
-<script type="text/javascript">
-
- function initMap() {
-  var map = createMap(53.594780, 9.884969);
-
-  /* AUTO COMPLETE SEARCH */ 
-  var input = document.getElementById('pac-input');
-  var autocomplete = new google.maps.places.Autocomplete(input);
-  autocomplete.bindTo('bounds', map);
-   /* END AUTOCOMPLETE SEARCH */ 
-
-  google.maps.event.addListener(autocomplete, 'place_changed', function () {
-      showResults(autocomplete);
-
-  });
-
-    $( "#distance" ).change(function() {
-      showResults(autocomplete);
-    });
-
-}
-
-function createMap(lat, lng) {
-  var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 11,
-      center: new google.maps.LatLng(lat, lng),
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      styles:  [
-  {
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "saturation": -70
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "color": "#93b8ca"
-      }
-    ]
-  }
-]
-    });
-   
-
-  return map;
-}
-
-function createMarker(map, data) {
-    var infowindow = new google.maps.InfoWindow();
-    var marker, i;
-
-     marker = new google.maps.Marker({
-          position: new google.maps.LatLng(data.lat, data.lng),
-          icon: 'images/'+data.mapicon,
-          map: map
-        });
-
-
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-          return function() {
-            for (var i = 0; i < infowindow.length; i++) {
-              infowindow[i].close();
-            }
-            infowindow.setContent(data.name + '<br><br>' + '<b>Address:</b><br>' + data.address);
-            infowindow.open(map, marker);
-          }
-        })(marker, i));
-
-
-}
-
-
-function showResults(autocomplete) {
-
-    var showmap = document.getElementById("inmap"); 
-    var place = autocomplete.getPlace(); 
-    var lat = place.geometry.location.lat();
-    var lng = place.geometry.location.lng();
-    document.getElementById("latitude").value = lat;
-    document.getElementById("longitude").value = lng;
-    dst = document.getElementById("distance").value;
-    dstToKm = dst / 1.62137119224; //convert km to miles
-    showmap.style.display = "block";
-
-    if((document.getElementById("latitude").value != '') && (document.getElementById("longitude").value != '')) {
-      $.ajax({
-              type: 'POST',
-              data: { latitude: lat,
-                      longitude: lng,
-                      distance: dstToKm,
-                  },
-              url: 'read.php',
-              dataType: 'json',
-              success: function(result){
-                    
-                    var map = createMap(lat, lng);
-
-                    $( ".collapsible" ).remove();
-                    $( ".content" ).remove();
-
-                    if(result.length > 0) {
-                        $.each(result, function(i, item) {
-                          /*var html ='<button class="collapsible" data-toggle="collapse" data-target="#collapse'+item.id+'" onclick="myFunction()"><img src="images/'+ item.logo +'"/><h3>'+ item.name +'</h3></button><div class="content" id="collapse'+item.id+'"><li class="dokimage"><img src="images/'+ item.image +'" /><br><br><span class="copy-text">'+ item.description +'</span><br><br><span class="copy-text">'+ item.address +'</span><br><br><span class="copy-text">Tel: '+ item.tel +'</span></li></div>';*/
-                          var html = '<button class="collapsible" onclick="myFunction()"><img src="images/'+ item.logo +'"/><h3>'+ item.name +'</h3></button> <div class="content"><li class="dokimage"><img src="images/'+ item.image +'" /><br><br><span class="copy-text">'+ item.description +'</span><br><br><span class="copy-text">'+ item.address +'</span><br><br><span class="copy-text">Tel: '+ item.tel +'</span><br><br><a href="mailto:'+ item.email +'"><i class="fas fa-envelope-square"></i></a>&nbsp;&nbsp;<a href="'+ item.website +'" target="_blank"><i class="fas fa-globe"></i></a> <a href="https://www.google.com/maps/?q='+ item.lat +','+ item.lng +'" target="_blank" class="mapdirection"><i class="fas fa-map-marker-alt"></i></a><br><br> <button class="closetab" onclick="myFunction()"><i class="fa fa-chevron-up" aria-hidden="true"></i></button></li></div>';
-                          $( ".leftinfolist" ).append( html );
-                          createMarker(map, item);
-                        });
-
-                    } else {
-                      var html ='<button class="collapsible"><h3>No results found</h3></button>';
-
-                        $( ".leftinfolist" ).append( html );
-                    }
-              },
-
-              error: function(){
-                  //window.alert("Wrong");
-                  console.log("Wrong");
-              }
-            });
-    }
-
-    /* SHOW MAP AFTER SELECTING ADDRESS */
-/*var btnact = document.getElementById("inmap");
-if (btnact.style.display === "none") {
-  btnact.style.display = "inline-block";
-}*/
-/* END SHOW MAP AFTER SELECTING ADDRESS */
-}
-
-</script>
-<!-- END CATEGORY MAP MARKERS -->
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDZnzboD5iPcg-PNQtY4vmWlQZ-9oCCTXc&libraries=places&style=element:geometry.fill%7Csaturation:-70&style=feature:water%7Celement:geometry.fill%7Ccolor:0x93b8ca&callback=initMap"></script>
-    
   </body>
 </html>
