2ae500b82cb87b9d1b294b701ea9f480944cab3e
Altrim <altr1m1@hotmail.com>
2018-06-03 12:00:21 +0200

    code changed

--- db/readdb.php
+++ db/readdb.php
@@ -5,26 +5,19 @@ $password = "";
 $database = "careforcancer";
 $tblName = "markers";
 $docs[] = "";
-/*$searchdok = $_GET['mapinput'];*/
 
 define("API_KEY","AIzaSyB9ZRlVlsODKlPKvWz5Bv0Y8b4gsmY3qZw");
 
 $conn = new mysqli($servername, $username, $password, $database);
-
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
 $docs = array();
 $res=$conn->query("SELECT * FROM $tblName");
-
 while ($data = $res->fetch_assoc()) {
     $docs[] = $data;
 }
-
 $docs = json_encode($docs);
-
 $conn->close();
-
 ?>
